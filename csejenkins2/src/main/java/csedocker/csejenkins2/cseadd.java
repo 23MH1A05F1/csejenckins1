44e7749b057093719fe4a92fd8e5943c193d362c
Gowthami Botsa <gowthami@example.com>
2026-02-23 14:31:59 +0530

    commit push

---
+++ csejenkins2/src/main/java/csedocker/csejenkins2/cseadd.java
@@ -0,0 +1,10 @@
+package csedocker.csejenkins2;
+
+public class cseadd {
+public int add(int a,int b) {
+	return a+b;
+}
+public int mul(int a, int b) {
+	return a*b;
+}
+}
---
+++ csejenkins2/src/test/java/csedocker/csejenkins2/cseaddTest.java
@@ -0,0 +1,26 @@
+package csedocker.csejenkins2;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CseaddTest2 {
+	Cseadd=new Cseadd();
+	
+}
+
+	@Test
+	void cseaddtest() {
+	int result =c.add(2,3);
+	assertEquals(5,result);
+	}
+	@Test
+	void csetest() {
+	int result =c.add(2,3);
+	assertEquals(6,result);
+	}
+
+}
